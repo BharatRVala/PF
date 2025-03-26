abe014f2372d3fccaae792bfb2b87a4ff945d439
Bharat vala <bharatkumarramashibhai2003@gmail.com>
2025-03-26 16:30:51 +0530

    Updated favicon

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,29 @@
 import { Route, Routes } from 'react-router-dom';
+import { useEffect } from 'react';
 import Home from './pagees/Home';
 import About from './pagees/About';
 import Contact from './pagees/Contact';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import logo from './assets/logo.png';
 
 function App() {
+  useEffect(() => {
+    const favicon = document.querySelector("link[rel='icon']");
+    if (favicon) {
+      favicon.href = logo;
+    }
+  }, []);
+
   return (
     <div className="min-h-screen flex flex-col">
-      <Header/>
+      <Header />
       <main className="flex-grow pt-16">
         <Routes>
-          <Route path='/' element={<Home />} /> 
-          <Route path='/home' element={<Home />} /> 
-          <Route path='/about' element={<About />} /> 
-          <Route path='/contact' element={<Contact />} /> 
+          <Route path="/" element={<Home />} /> 
+          <Route path="/home" element={<Home />} /> 
+          <Route path="/about" element={<About />} /> 
+          <Route path="/contact" element={<Contact />} /> 
         </Routes>
       </main>
       <Footer />
